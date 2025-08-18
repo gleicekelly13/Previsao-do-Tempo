e84e07570097321c20eb0c153571f542b1691e50
gleicekelly13 <gleice.kelly22@hotmail.com>
2025-08-18 17:03:24 -0300

    style: Aplica estilo inicial na div principal do card de clima

--- src/components/PrevisaoDoTempo.jsx
+++ src/components/PrevisaoDoTempo.jsx
@@ -52,7 +52,7 @@ function PrevisaoDoTempo () {
             </button>
     
             {clima && (
-              <div>
+              <div className='mt-6 bg-sky-400 shadow-lg rounded-2xl p-6 w-80 text-center leading-10'>
                 <h2>{clima.name}</h2>
                 <p>{clima.weather[0].description}</p>
                 <p>Temperatura: {clima.main.temp}°C</p>
